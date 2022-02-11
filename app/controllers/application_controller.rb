6c1b536490c16b6d680e2dfb346b1aad013279e3
Clay Stewart <c.stewart89@gmail.com>
2022-02-10 21:23:33 -0500

    Passed test for set up user and sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  private
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    User.find_by_id(session[:user_id]) || nil
+  end
+
+  def require_login
+    redirect_to '/' unless logged_in?
+  end
 end
